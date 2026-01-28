52ff15b11829623c2b7b061305061ed3beff88fe
nicolasspaezt <santiagopaez995@gmail.com>
2026-01-28 10:35:05 -0500

    CODE Fizzbuzz Solution

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+for number in range(1, 1001):
+  if number % 3 == 0 and number % 5 == 0:
+    if number % 3 == 0:
+     print("Fizz")
+  elif number % 5 == 0:
+   print("Buzz")
+else:
+   print("FizzBuzz")
+print(number)
+
+"""
+
+
+for number in range(1, 1001):
+ if number % 3 == 0 and number % 5 == 0:
+        print('Fizzbuzz')
+ elif number % 3 == 0:
+        print('Fizz')
+ elif number % 5 == 0:
+        print('Buzz')
+ else:
+        print(number)
+
